1706d7767aac79366510240b953f3871ea60bd6d
Kakashi-sama <samratdps@gmai.com>
2020-12-12 19:57:00 +0530

    updated

--- basic_motion_detection.py
+++ basic_motion_detection.py
@@ -10,6 +10,7 @@ while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imshow('test', gray)
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
